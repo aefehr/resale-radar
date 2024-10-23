6c52acbcb5edba995de3035f1a806f46fb077824
aefehr <fehr.allie@gmail.com>
2024-10-23 10:54:04 -0400

    Add title and subtitle to landing

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 const LandingPage = () => {
     return (
         <div className="landing">
-            <h1>I'm a...</h1>
-            <Link to="/buyer">
-                <button>Buyer</button>
-            </Link>
-            <Link to="/seller">
-                <button>Seller</button>
-            </Link>
+            <h1>Resale Radar</h1>
+            <h3>An e-commerce guide for second-hand buyers and sellers</h3>
+            <h2>I'm a...</h2>
+            <div className='button-container'> 
+                <Link to="/buyer">
+                    <button>Buyer</button>
+                </Link>
+                <Link to="/seller">
+                    <button>Seller</button>
+                </Link>
+            </div>
         </div>
     );
 };
